1fedd038d9097624fdeaa627f0a48e62c0e84563
CHonesetDoPa <ch@nekoc.cc>
2023-01-07 17:03:37 +0800

    update

---
+++ api.js
@@ -0,0 +1,7 @@
+var version = "Game Version v26";
+var apiversion = "Api Version 1.0";
+const unixtime = Date.now();
+
+document.getElementById('version').innerText= version;
+document.getElementById('apiversion').innerText= apiversion;
+document.getElementById('unixtime').innerText= "UnixTime " + unixtime;
